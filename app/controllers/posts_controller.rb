d0b10ee339c1f8c332105c67d1988c7f0c6cbd51
Menekam Rudy <menekamrudy@gmail.com>
2023-05-20 17:50:18 +0100

    Add form

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,5 @@
 class PostsController < ApplicationController
+  protect_from_forgery prepend: true
   def index
     @user = User.find(params[:user_id])
     @posts = @user.posts
@@ -6,5 +7,31 @@ class PostsController < ApplicationController
 
   def show
     @post = Post.find(params[:id])
+  end
+
+  def new
+    @post = @current_user.posts.new
+  end
+
+  def create
+    @user = User.find(params[:user_id])
+    @post = @user.posts.new(post_params)
+    @post.commentscounter = 0
+    @post.likescounter = 0
+    respond_to do |format|
+      format.html do
+        if @post.save
+          redirect_to user_posts_path(@user)
+        else
+          render :new, locals: { post: @post }
+        end
+      end
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :text)
   end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,8 +1,8 @@
 class Post < ApplicationRecord
-  belongs_to :author, class_name: 'User', counter_cache: :postscounter
-  has_many :comments, dependent: :destroy
-  has_many :likes, dependent: :destroy
-  after_save :update_post_counter
+  belongs_to :author, class_name: 'User'
+  has_many :comments, foreign_key: 'author_id', dependent: :destroy
+  has_many :likes, foreign_key: 'author_id', dependent: :destroy
+  # after_save :update_post_counter
 
   validates :title, presence: true, length: { maximum: 250 }
   validates :commentscounter, numericality: { only_integer: true }, comparison: { greater_than_or_equal_to: 0 }
