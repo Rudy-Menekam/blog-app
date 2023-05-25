a22751ee7e3dac27a73e854e96f2cce9b8ea54be
Menekam Rudy <menekamrudy@gmail.com>
2023-05-25 23:06:58 +0100

    Remove commented code

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,6 @@ class User < ApplicationRecord
   has_many :likes, foreign_key: 'author_id', dependent: :destroy
 
   validates :name, presence: true
-  # validates :postscounter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
   def most_recent_posts
     Post.limit(3).order(created_at: :desc).where(author: self)
